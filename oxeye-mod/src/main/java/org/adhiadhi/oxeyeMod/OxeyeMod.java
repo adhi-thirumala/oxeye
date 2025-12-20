516e772c64b2c330cd7530c888f0a03c7255fd76
adhi <me@adhithirumala.com>
2025-12-20 15:27:14 -0500

    finally added all relevant things

---
+++ oxeye-mod/src/main/java/org/adhiadhi/oxeyeMod/OxeyeMod.java
@@ -0,0 +1,12 @@
+package org.adhiadhi.oxeyeMod;
+
+import net.fabricmc.api.ModInitializer;
+import static net.minecraft.server.commands.
+
+
+public class OxeyeMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
